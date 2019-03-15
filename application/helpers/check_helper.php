54bc8d4cf167115cdc9b8f7f5c59fcfe76df47e6
lzdszdl <136208872@qq.com>
2019-03-15 16:16:53 +0800

    Update check_helper.php

--- application/helpers/check_helper.php
+++ application/helpers/check_helper.php
@@ -73,7 +73,7 @@
     }
     //获取服务器IP
     function serverip(){
-        $curl = curl_init("https://ip.awk.sh/api.php?data=ip");
+        $curl = curl_init("http://whatismyip.akamai.com/");
 
         curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36");
         curl_setopt($curl, CURLOPT_FAILONERROR, true);
